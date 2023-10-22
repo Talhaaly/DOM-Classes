19e3319b80c6be1f32e4f2fc8855454ae2574bcb
Talha Ali Akber <talhaaly103@gmail.com>
2023-10-22 21:59:28 +0500

    js

--- DOM1.js
+++ DOM1.js
@@ -3,20 +3,77 @@ const btnArr = [
 ];
 
 const menuCard = [
-
+{
+    title: "d",
+    title: "s",
+    title: "c",
+    title: "d",
+    title: "e"
+},
+{
+    title: "d",
+    title: "s",
+    title: "c",
+    title: "d",
+    title: "e"
+},
+{
+    title: "d",
+    title: "s",
+    title: "c",
+    title: "d",
+    title: "e"
+},
+{
+    title: "d",
+    title: "s",
+    title: "c",
+    title: "d",
+    title: "e"
+},
+{
+    title: "d",
+    title: "s",
+    title: "c",
+    title: "d",
+    title: "e"
+},
+{
+    title: "Cake",
+    price: "500",
+    desc: "mujhy khila do",
+    src: "d",
+    category: "shake"
+}
 ]
 
 const menuBtns = document.querySelector('.btn')
+const mainMenu = document.querySelector('.menu')
 
 btnArr.forEach((elem)=>{
-    menuBtns.innerHTML += `<button type="button" class="btn btn-outline-warning" onclick="${elem}">${elem}</button>`
+    menuBtns.innerHTML += `<button type="button" class="btn btn-outline-warning" onclick="${elem}()">${elem}</button>`
 
 })
+
+// function printData(){
+//     menuCard.forEach(()=>{
+//         mainMenu.innerHTML += ``
+//     })
+// }
 
 
 
 function All(){
-    console.log("tukka");
+    console.log("All tukka");
+}
+function Breakfast(){
+    console.log("Breakfast tukka");
+}
+function Lunch(){
+    console.log(" Lunch tukka");
+}
+function Shakes(){
+    console.log("Shakes tukka");
 }
 
 
